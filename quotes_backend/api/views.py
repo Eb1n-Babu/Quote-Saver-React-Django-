4b2262e6109e97f2c3cd0f2a26b644ac3fe8a8b9
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-24 22:38:46 +0530

    view.py implemented and updated with quotes view set

--- quotes_backend/api/views.py
+++ quotes_backend/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import QuoteModel
+from api.serializers import QuoteSerializer
+
 
 # Create your views here.
+class QuotesViewSet(viewsets.ModelViewSet):
+    queryset = QuoteModel.objects.all()
+    serializer_class = QuoteSerializer
